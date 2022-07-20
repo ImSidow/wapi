14213aee84f3a30abd869349eafa4c09df55e7b5
ImSidow <sidow.code@gmail.com>
2022-07-20 19:54:56 +0300

    modified

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Imsidow\Wapi\Provider;
+use Imsidow\Wapi\Provider; 
 
 $wapi = new Provider('M0910278', '1000389', 'API-2126079682AHX');
 
