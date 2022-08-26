a44b519f721e7a1a87203c8bf2e93a643c1c6b88
ImSidow <sidow.code@gmail.com>
2022-08-26 18:26:44 +0300

    bug fixed

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@ $wapi = new Provider('M0910278', '1000389', 'API-2126079682AHX');
 $wapi->requestMobilePayment([
     "mobile" => "252616505113",
     "reference" => '0001',
-    "invoiceNo" => "00001",
+    "invoiceNo" => "00021",
     // "amount" => 0.1 
 ]);
 
