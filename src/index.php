1428489bdcbe62b56d4dc1655eed47c24e7ac1c2
ImSidow <sidow.code@gmail.com>
2022-08-26 18:25:27 +0300

    last update

--- src/index.php
+++ src/index.php
@@ -9,7 +9,7 @@ $wapi->requestMobilePayment([
     "mobile" => "252616505113",
     "reference" => '0001',
     "invoiceNo" => "00001",
-    // "amount" => 0.1
+    // "amount" => 0.1 
 ]);
 
 // $wapi->refundPayment([
